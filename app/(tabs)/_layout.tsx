9f90eabd3000d40fc7de7fadf3a77c7b16ac22eb
Dmitry Eshman <bakko7821@gmail.com>
2026-09-11 12:53:22 +0300

    fix nativewind bug and install router

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+
+      <Tabs.Screen
+        name="library"
+        options={{
+          title: "Library",
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+        }}
+      />
+    </Tabs>
+  );
+}
